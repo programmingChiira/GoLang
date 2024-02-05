ac12090c2624c17e6eeae45a20f090e08b1c49fe
DennisChiira <programmingchiira@gmail.com>
2024-02-05 11:19:37 +0300

    using two go files

---
+++ hello1.go
@@ -0,0 +1,8 @@
+// hello1.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there 1! - File 1")
+}
---
+++ hello2.go
@@ -0,0 +1,8 @@
+// hello1.go
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there 2! - File 2")
+}
