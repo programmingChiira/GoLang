cc5ceb97dfe673e4b067bdd9aea0c863a70d70d5
DennisChiira <programmingchiira@gmail.com>
2024-02-05 12:57:36 +0300

    using the Print function

---
+++ printFunction.go
@@ -0,0 +1,12 @@
+// The Print() function prints its arguments with their default format.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i, j string = "Hello", "World!"
+
+	fmt.Print(i, j)
+}
