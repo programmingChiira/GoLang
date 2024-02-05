c65d0bda067ef2edb2ad5bb8d91e8ac0a04482f8
DennisChiira <programmingchiira@gmail.com>
2024-02-05 14:18:24 +0300

    explicit array declaration

---
+++ explicitArrayDeclaration.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var array1 = [4]int{12, 23, 34, 45}
+	var array2 = [5]int{32, 36, 77, 11, 90}
+
+	fmt.Println(array1)
+	fmt.Println(array2)
+}
