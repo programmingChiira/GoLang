099b4f314e161b6ade30cfa977e0e6d2078a1c8a
DennisChiira <programmingchiira@gmail.com>
2024-02-05 11:35:03 +0300

    shows just the different syntaxes for variable declaration

---
+++ variableDeclaration.go
@@ -0,0 +1,21 @@
+// In Go, there are different types of variables, for example:
+
+// int- stores integers (whole numbers), such as 123 or -123
+// float32- stores floating point numbers, with decimals, such as 19.99 or -19.99
+// string - stores text, such as "Hello World". String values are surrounded by double quotes
+// bool- stores values with two states: true or false
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Variable decalration")
+	// the syntax for variable declaration is as below
+	// var variablename type = value
+
+	// Using the := sign is as below
+	// variablename := value
+
+}
