c0c27fa75c76914ae3ae8ed5eb355b42dbb8effc
DennisChiira <programmingchiira@gmail.com>
2024-02-05 11:41:15 +0300

    variable declaration with initial value

---
+++ variableDeclarationWithInitialValue.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var string1 string = "String one" // This is a string
+
+	var string2 = "String two" // The string is inferred
+
+	var number = 12
+
+	fmt.Println(string1)
+	fmt.Println(string2)
+	fmt.Println(number)
+}
