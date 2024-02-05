76b4ed3b9f7c1941d1f6064f89cc9eb81b014457
DennisChiira <programmingchiira@gmail.com>
2024-02-05 14:21:39 +0300

    implicit array declaration

---
+++ implicitArrayDeclaration.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var array1 = [...]int{2, 45, 23, 135}
+	var array2 = [...]int{3, 45, 23, 67, 89, 778}
+
+	fmt.Println(array1)
+	fmt.Println(array2)
+}
