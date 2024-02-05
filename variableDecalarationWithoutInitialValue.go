de611f4f4ebf33a7751c08dddeba430cbaa0cdfb
DennisChiira <programmingchiira@gmail.com>
2024-02-05 11:49:03 +0300

    variable declaration without value

---
+++ variableDecalarationWithoutInitialValue.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a string
+	var b int
+	var c bool
+	var d float32
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
