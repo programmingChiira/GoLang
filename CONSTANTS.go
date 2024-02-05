6ae8244961c5abbe4f7d7627832742c1bb139d8e
DennisChiira <programmingchiira@gmail.com>
2024-02-05 12:30:42 +0300

    using constants in Golang

---
+++ CONSTANTS.go
@@ -0,0 +1,12 @@
+// constans in go are similar to variables, only that they use uppercase names and use the const keyword
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const NAME = "This is for the constant name"
+
+	fmt.Println(NAME)
+}
